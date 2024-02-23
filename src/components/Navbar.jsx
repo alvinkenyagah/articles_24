f2c00ccb14e54df7157c01243aefa0adc8d39985
alvinkenyagah <alvin47k@gmail.com>
2024-02-23 13:20:09 +0300

    Starter template

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './navbar.css'
+
+
+export function Navbar(){
+
+
+return(
+
+<ul>
+    <li>HOME</li>
+    <li>ABOUT</li>
+    <li>CONTACT US</li>
+    <li>SERVICES</li>
+</ul>
+
+
+)
+
+
+
+}
